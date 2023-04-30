b6008da0d73054a4c1007b587fe23cf0ea06a7e7
Emily Le <131802939+emilyle91@users.noreply.github.com>
2023-04-30 23:23:19 +0700

    state the hypothesis: stroke people have higher BMI; initial calculation

--- stroke-prediction-analysis-script.R
+++ stroke-prediction-analysis-script.R
@@ -1,6 +1,24 @@
-#remove any variable before start new script
+#Remove any variable before start new script
 rm(list = ls())
 
-#import data
-strokedata <- read.csv("C:/Users/emily/OneDrive/Tài liệu/Project/Project 2 (Stroke)/Working File/healthcare-dataset-stroke-data.csv")
+#Import data
+strokedata <- read.csv("healthcare-dataset-stroke-data.csv")
 
+#Import package
+library(Hmisc)
+
+#Hmisc command to take a brief look on the data set
+describe(strokedata)
+
+#Hypothesis: people who had stroke is higher in bmi than people who had no stroke.
+#Create two table: stroke people, normal people
+strokepeople = subset(strokedata, strokedata$stroke == 1)
+normalpeople = subset(strokedata, strokedata$stroke == 0)
+
+#Turn bmi character into numeric data type
+strokepeople$bmi <- as.numeric(strokepeople$bmi)
+normalpeople$bmi <- as.numeric(normalpeople$bmi)
+
+#calculate mean BMI of people has stroke & mean BMI of people has no stroke
+mean(strokepeople$bmi, na.rm = TRUE)
+mean(normalpeople$bmi, na.rm = TRUE)
