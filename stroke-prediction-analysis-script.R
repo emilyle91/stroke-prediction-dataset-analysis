d5a8a1826614a65d336f03eece3962b41a66f901
Emily Le <131802939+emilyle91@users.noreply.github.com>
2023-04-30 20:47:45 +0700

    import dataset

---
+++ stroke-prediction-analysis-script.R
@@ -0,0 +1,6 @@
+#remove any variable before start new script
+rm(list = ls())
+
+#import data
+strokedata <- read.csv("C:/Users/emily/OneDrive/Tài liệu/Project/Project 2 (Stroke)/Working File/healthcare-dataset-stroke-data.csv")
+
