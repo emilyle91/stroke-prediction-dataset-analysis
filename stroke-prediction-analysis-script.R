1caa67cd7428ddc4cd4c6888e7b6ead2eccb979e
Emily Le <131802939+emilyle91@users.noreply.github.com>
2023-05-01 18:02:04 +0700

    Draw the gender distribution of the stroke dataset pie chart

--- stroke-prediction-analysis-script.R
+++ stroke-prediction-analysis-script.R
@@ -39,24 +39,42 @@ ggplot(data = strokepeople, mapping = aes(x = age, y = bmi)) +
   geom_point() +
   geom_smooth()
 
+ggplot(data = normalpeople, mapping = aes(x = age, y = bmi)) +
+  geom_point() +
+  geom_smooth()
+
 ggplot(data = strokepeople) +
   geom_point(mapping = aes(x = age, y = bmi), colour = "blue", pch = "+") +
   labs(title = "The BMI pattern of stroke people", x = "Age of stroke people",
        y = "BMI of stroke people")
-
-
-ggplot(data = normalpeople, mapping = aes(x = age, y = bmi)) +
-  geom_point() +
-  geom_smooth()
 
 ggplot(data = normalpeople) +
   geom_point(mapping = aes(x = age, y = bmi), colour = "blue", pch = "+") +
   labs(title = "The BMI pattern of normal people", x = "Age of normal people",
        y = "BMI of normal people")
 
-
 #Conclusion from chart: 
 #number of normal people is greater than stroke people
 #the average bmi of stroke people (30) is higher than average bmi of normal people (28)
 #people older than 40 years old + BMI level higher than average of 25 is likely to encounter stroke
+
+#Create pie chart table
+#a) Gender Distribution in Stroke Dataset
+
+#Calculate frequency of each gender in the stroke dataset
+gender_counts <- table(strokepeople$gender)
+
+#Convert gender_counts table to a dataframe
+gender_df <- as.data.frame(gender_counts)
+
+#Rename columns within the dataframe
+colnames(gender_df) <- c("gender", "count")
+
+#Create pie chart
+ggplot(gender_df, aes(x="", y=count, fill=gender)) + 
+  geom_bar(stat="identity", width=1) +
+  coord_polar(theta="y") +
+  labs(title="Gender Distribution in Stroke Dataset") +
+  scale_fill_manual(values=c("pink", "blue")) +
+  theme_void()
 
